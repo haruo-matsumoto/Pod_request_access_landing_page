5a4c258f3f5e45b9a9b9f226e96a8d4e11907199
Haruo <mrobsonharuo@gmail.com>
2021-03-31 16:05:06 -0300

    Create email validation and message alert

--- assets/JS/access.js
+++ assets/JS/access.js
@@ -1,3 +1,13 @@
-function requestAccess(){
-    console.log(clicou)
+ function requestAccess() {
+    var  email = document.getElementById("email");
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (email.value == ""){
+        alert('Oops! Please add your email');
+        return;
+    }else if (email.value.match(mailformat)){
+        alert('Successfully accessed');
+    }else {
+        alert("Oops! Please check your email")
+    }  
 }
